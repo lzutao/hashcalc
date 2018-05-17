3800f3940a94e29b0eb7a1531de6be21a5df33ec
Lzu Tao <1stpangu@gmail.com>
2018-05-17 08:37:13 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# hashcalc
+
+Print MD5 (128-bit) or SHA1 (160-bit) checksum.
+
+## Dependencies
+hashcalc currently needs following libraries:
+- python2 (>= 2.7) or python3 (>= 3.5)
+- python future
+- python six
+
+On Ubuntu 16.04 you can easily install them like this:
+```bash
+sudo apt-get update
+sudo apt-get install python{,3}-{future,six}
+```
+
+## Usage
+
+The latest version can be downloaded via GitHub.
+```bash
+git clone https://github.com/lzutao/hashcalc --depth 1
+cd hashcalc
+```
+
+To print MD5 checksum from stdin:
+```bash
+echo input | python main.py
+```
+
+To print MD5 checksum from file:
+```bash
+python main.py <inputfile>
+```
+
+If input is encoded in hex:
+```bash
+echo CAFEBABE | python main.py -x
+```
+
+Add `-m sha1` to get SHA1 checksum in above examples. Example:
+```bash
+echo CAFEBABE | python main.py -x -m sha1
+```
+to print SHA1 checksum where input from stdin and encoded in hex.
+
+After installing required libraries, you are ready to use or install this program.
+
+## License
+Licensed under [MIT](COPYING).
+
+## TODO
+
+Read more in [TODO](https://github.com/lzutao/hashcalc/projects) at GitHub's Projects.
+
+[how-to-clone]: https://help.github.com/articles/cloning-a-repository/
+[release]: https://github.com/lzutao/hashcalc/releases
---
+++ main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from builtins import bytes, str
+
+import argparse
+import logging
+import sys
+
+# local import
+import hashcalc
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(name)s: %(message)s',
+)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="hashcalc",
+        usage='%(prog)s [OPTION]... [FILE]',
+        description='Print MD5 (128-bit) or SHA1 (160-bit) checksum.',
+        epilog="[+] Written by 15520599"
+        )
+    parser.add_argument(
+        'FILE',
+        nargs='?',
+        type=argparse.FileType('rb'),
+        default=sys.stdin,
+        help="With no FILE, or when FILE is -, read from standard input."
+        )
+    parser.add_argument(
+        '-m', '--mode',
+        help='Choose hash algorithm: MD5 or SHA1 (Default MD5).',
+        choices=['md5', 'sha1']
+        )
+    parser.add_argument(
+        '-x', '--hex',
+        action="store_true",
+        help='Read input encoded in hex mode.',
+        )
+
+    args = parser.parse_args()
+    logger.debug(args.FILE)
+
+    hash_func = None
+    file_name = None
+    content = None
+
+    if not args.mode:
+        hash_func = 'md5'
+    else:
+        hash_func = args.mode
+
+    # if input is stdin or input encoded in hex
+    if (args.FILE is sys.stdin) or args.hex:
+        # be careful with newline character
+        content = args.FILE.read()
+        if args.hex:
+            content = bytes.fromhex(content)
+        hash_func += 'sumhex'
+    else:
+        # read from opened file
+        hash_func += 'filehex'
+        file_name = args.FILE.name
+
+    hash_func = getattr(hashcalc, hash_func)
+
+    logger.debug("hash func: %r"%hash_func)
+
+    checksum = None
+    if file_name:
+        checksum = hash_func(file_name)
+    else:
+        checksum = hash_func(content)
+
+    print("%s"%checksum)
+
+
+if __name__ == '__main__':
+    main()
+
